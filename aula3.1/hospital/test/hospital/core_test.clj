ddc0286e0fe2527019e99cb364f399bd85f035c8
caiocmsouza <caiocesar.msouza@gmail.com>
2019-08-26 12:28:47 -0300

    branch aula só com aula2

--- aula3.1/hospital/test/hospital/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hospital.core-test
-  (:require [clojure.test :refer :all]
-            [hospital.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- aula6.2/hospital/src/hospital/model.clj
+++
@@ -1,9 +0,0 @@
-(ns hospital.model)
-
-(def fila-vazia clojure.lang.PersistentQueue/EMPTY)
-
-(defn novo-hospital []
-  {:espera       fila-vazia
-   :laboratorio1 fila-vazia
-   :laboratorio2 fila-vazia
-   :laboratorio3 fila-vazia})
